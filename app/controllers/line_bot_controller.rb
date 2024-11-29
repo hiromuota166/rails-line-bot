3c313764cf012e308b1816c87d4223bae2113c0d
hiromuota166 <otahiromu166@gmail.com>
2024-11-30 02:37:35 +0900

    色々変更

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -4,12 +4,20 @@ class LineBotController < ApplicationController
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
 
+    logger.debug "Received signature: #{signature}"
+
     if !validate_signature(body, signature)
       head :bad_request
       return
     end
 
-    events = JSON.parse(body)['events']
+    begin
+      events = JSON.parse(body)['events']
+    rescue JSON::ParserError => e
+      logger.error "JSON Parsing Error: #{e.message}"
+      head :bad_request
+      return
+    end
 
     events.each do |event|
       case event['type']
@@ -26,6 +34,9 @@ class LineBotController < ApplicationController
   def handle_message(event)
     message = event['message']
     reply_token = event['replyToken']
+
+    logger.debug "Received message: #{message}"
+    logger.debug "Reply token: #{reply_token}"
 
     if message['type'] == 'text'
       reply_message(reply_token, "You said: #{message['text']}")
@@ -47,10 +58,21 @@ class LineBotController < ApplicationController
       messages: [{ type: 'text', text: text }]
     }.to_json
 
-    http.request(request)
+    response = http.request(request)
+
+    logger.debug "LINE API response: #{response.code} - #{response.body}"
+
+    if response.code.to_i != 200
+      logger.error "Failed to send message: #{response.body}"
+    end
   end
 
   def validate_signature(body, signature)
+    if signature.nil?
+      logger.error "Signature is nil"
+      return false
+    end
+
     expected_signature = OpenSSL::HMAC.hexdigest('SHA256', ENV['LINE_CHANNEL_SECRET'], body)
     signature == expected_signature
   end
