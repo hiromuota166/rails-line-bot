1bad3ca446d1c48f2fc2cc5f6a2878f8d893885c
hiromuota166 <otahiromu166@gmail.com>
2024-11-30 09:47:03 +0900

    fix: 最低限に変更

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -5,21 +5,6 @@ class LineBotController < ApplicationController
 
   def webhook
     body = request.body.read
-    # signature = request.env['HTTP_X_LINE_SIGNATURE']
-
-    # logger.debug "Received signature: #{signature}"
-
-    # if signature.nil?
-    #   logger.error "Signature is nil"
-    #   head :bad_request
-    #   return
-    # end
-
-    # if !validate_signature(body, signature)
-    #   logger.error "Signature validation failed"
-    #   head :bad_request
-    #   return
-    # end
 
     events = JSON.parse(body)['events']
     events.each do |event|
@@ -37,9 +22,6 @@ class LineBotController < ApplicationController
   def handle_message(event)
     message = event['message']
     reply_token = event['replyToken']
-
-    logger.debug "Received message: #{message}"
-    logger.debug "Reply token: #{reply_token}"
 
     if message['type'] == 'text'
       reply_message(reply_token, "You said: #{message['text']}")
@@ -62,21 +44,5 @@ class LineBotController < ApplicationController
     }.to_json
 
     response = http.request(request)
-
-    logger.debug "LINE API response: #{response.code} - #{response.body}"
-
-    if response.code.to_i != 200
-      logger.error "Failed to send message: #{response.body}"
-    end
   end
-
-  # def validate_signature(body, signature)
-  #   if signature.nil?
-  #     logger.error "Signature is nil"
-  #     return false
-  #   end
-
-  #   expected_signature = OpenSSL::HMAC.hexdigest('SHA256', ENV['LINE_CHANNEL_SECRET'], body)
-  #   signature == expected_signature
-  # end
 end
